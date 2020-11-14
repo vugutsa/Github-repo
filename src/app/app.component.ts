e3bfb26d737b9d5e46f523f92eaf940ddf4ba436
vugutsa <vugutsamercy84@gmail.com>
2020-11-14 10:49:56 +0300

    add the search component to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { GithubReposService } from './github-repos.service';
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'Github-repos';
 
   gitHubRepositories: any = [];
+  mySubs: Subscription;
   constructor(private myServiceRepo: GithubReposService){
   this.getRepositories();
+  }
+  ngOnDestroy(): void {
+   this.mySubs.unsubscribe();
   }
   ngOnInit(): void {
     this.getRepositories();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule  } from "@angular/common/http";
+import { SearchComponent } from './search/search.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
