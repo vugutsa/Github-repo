a891076e2d4722b57af495afff32b4001c714d85
vugutsa <vugutsamercy84@gmail.com>
2020-11-16 09:03:19 +0300

    add the nav bar to display

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GithubReposComponent } from './github-repos/github-repos.component';
+const routes: Routes = [
+  { path: 'github-repos', component: GithubReposComponent  },
+  { path: '', redirectTo: "/github-repos", pathMatch:"full"},
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
