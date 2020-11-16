cc2f6d9461782b63e1edbada30b8cb415f69c0ed
vugutsa <vugutsamercy84@gmail.com>
2020-11-16 09:28:19 +0300

    add the transformof the date

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,8 +5,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+  // transform(value: unknown, ...args: unknown[]): unknown {
+  //   return null;
+  // }
+  transform(value: any): number {
+    const today: Date = new Date();
+    const todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    // tslint:disable-next-line: prefer-const
+    let dateDifference = Math.abs(value - todayWithNoTime);
+    const secondsInDay = 86400;
+    const dateDifferenceSeconds = dateDifference * 0.001;
+    // tslint:disable-next-line: prefer-const
+    let dateCounter = dateDifferenceSeconds / secondsInDay;
 
+  //   if (dateCounter >= 1 && value < todayWithNoTime){
+  //     return dateCounter;
+  //   }else{
+  //     return 0;
+  //   }
+  // }
 }
